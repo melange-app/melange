ed00f531afbeb8a60fed4b0f661d3ddc7eb2ca4c
Hunter Leath <h@hunterleath.com>
2014-03-21 16:43:14 -0400

    Added Application Error Messages

--- app/controllers/loader.go
+++ app/controllers/loader.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	"github.com/airdispatch/dpl"
 	"github.com/robfig/revel"
-	"io/ioutil"
 	//"melange/app/routes"
+	"melange/app/models"
+	"net/http"
 	"net/url"
-	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -92,18 +92,33 @@ func (d Loader) LoadAppUser(app string, action string, username string) revel.Re
 
 // We should load the Application just once from the XML, then refresh it at a regular interval
 func (d Loader) LoadApp(app string, action string, message dpl.Message, user *dpl.User) revel.Result {
-	xmlFile, err := os.Open("../github.com/airdispatch/dpl/example/" + app + ".dpl")
+	var apps []*models.UserApp
+	_, err := d.Txn.Select(&apps, "select * from dispatch_app where userid = $1 and UPPER(name) = UPPER($2)", GetUserId(d.Session), app)
 	if err != nil {
-		panic("Couldn't find App")
+		panic(err)
 	}
-	defer xmlFile.Close()
+	if len(apps) != 1 {
+		panic(len(apps))
+	}
 
-	b, _ := ioutil.ReadAll(xmlFile)
+	resp, err := http.Get(apps[0].AppURL)
+	if err != nil {
+		d.RenderArgs = map[string]interface{}{
+			"error": "We were unable to load this application from the supplied URL. Maybe it has moved or the URL was incorrect?",
+			"app":   apps[0],
+		}
+		return d.RenderTemplate("loader/error.html")
+	}
+	defer resp.Body.Close()
 
 	// The Plugin is O
-	o, err := dpl.ParseDPL(b)
+	o, err := dpl.ParseDPLStream(resp.Body)
 	if err != nil {
-		panic("Couldn't Parse App: " + err.Error())
+		d.RenderArgs = map[string]interface{}{
+			"error": "This plugin is no longer valid. You should contact the plugin developer.",
+			"app":   apps[0],
+		}
+		return d.RenderTemplate("load/error.html")
 	}
 
 	// Create a Singleton that Hosts all the Plugins
