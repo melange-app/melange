f1a3e711fbb75623bf49f2468a078aa36edfbea6
Hunter Leath <h@hunterleath.com>
2014-07-08 12:45:40 -0400

    Added DAP Protocol Buffers and Codes

---
+++ server/dap/wire/wire.go
@@ -0,0 +1,17 @@
+package wire
+
+const (
+	// Account Management
+	RegisterCode   = "DAP-REG"
+	UnregisterCode = "DAP-URG"
+	// Message Management
+	DownloadMessagesCode = "DAP-MSG"
+	PublishMessageCode   = "DAP-PUB"
+	UpdateMessageCode    = "DAP-UPD"
+	// Data Management
+	DataCode    = "DAP-DAT"
+	GetDataCode = "DAP-GET"
+	// Responses
+	ResponseCode        = "DAP-RES"
+	ResponseMessageCode = "DAP-RMS"
+)
