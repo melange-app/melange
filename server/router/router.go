7bccb162085adb804b37642d52b367de048fdb1b
Hunter Leath <h@hunterleath.com>
2014-07-31 13:36:44 -0400

    Moved melange/router to new Routing Framework

--- server/router/router.go
+++ server/router/router.go
@@ -3,12 +3,14 @@ package router
 // This file describes two routes that are used throughout the Melange System.
 
 import (
-	"airdispat.ch/identity"
-	"airdispat.ch/tracker"
 	"errors"
 	"fmt"
-	"github.com/huntaub/go-cache"
 	"strings"
+
+	"airdispat.ch/identity"
+	"airdispat.ch/routing"
+	"airdispat.ch/tracker"
+	cache "github.com/huntaub/go-cache"
 )
 
 var routeCache *cache.Cache
@@ -20,16 +22,32 @@ var ServerKey *identity.Identity
 type Router struct {
 	Origin      *identity.Identity
 	TrackerList []string
+	Redirects   int
 }
 
-func (a *Router) LookupAlias(from string) (*identity.Address, error) {
+func (r *Router) HandleRedirect(name routing.LookupType, redirect routing.Redirect) (*identity.Address, error) {
+	r.Redirects++
+	if r.Redirects > 10 {
+		return nil, errors.New("Too many redirects.")
+	}
+	data, err := r.LookupAlias(redirect.Alias, name)
+	if err != nil {
+		return nil, err
+	}
+	if data.String() != redirect.Fingerprint {
+		return nil, errors.New("Redirected data doesn't have the same fingerprint.")
+	}
+	return data, nil
+}
+
+func (a *Router) LookupAlias(from string, name routing.LookupType) (*identity.Address, error) {
 	test, stale := routeCache.Get(from)
 	if !stale {
 		return test.(*identity.Address), nil
 	}
 
 	if from[0] == '/' {
-		return a.Lookup(from[1:])
+		return a.Lookup(from[1:], name)
 	}
 	comp := strings.Split(from, "@")
 	if len(comp) != 2 {
@@ -37,9 +55,13 @@ func (a *Router) LookupAlias(from string) (*identity.Address, error) {
 	}
 
 	url := tracker.GetTrackingServerLocationFromURL(comp[1])
-	t := &tracker.TrackerRouter{url, a.Origin}
+	t := &tracker.Router{
+		URL:        url,
+		Origin:     a.Origin,
+		Redirector: a,
+	}
 
-	addr, err := t.LookupAlias(comp[0])
+	addr, err := t.LookupAlias(comp[0], name)
 	if err == nil {
 		routeCache.Store(from, addr)
 		routeCache.Store(addr.String(), addr)
@@ -48,7 +70,7 @@ func (a *Router) LookupAlias(from string) (*identity.Address, error) {
 	return nil, err
 }
 
-func (a *Router) Lookup(from string) (*identity.Address, error) {
+func (a *Router) Lookup(from string, name routing.LookupType) (*identity.Address, error) {
 	test, stale := routeCache.Get(from)
 	if !stale {
 		return test.(*identity.Address), nil
@@ -56,7 +78,11 @@ func (a *Router) Lookup(from string) (*identity.Address, error) {
 
 	for _, v := range a.TrackerList {
 		fmt.Println(v, ServerKey)
-		a, err := (&tracker.TrackerRouter{v, ServerKey}).Lookup(from)
+		a, err := (&tracker.Router{
+			URL:        v,
+			Origin:     ServerKey,
+			Redirector: a,
+		}).Lookup(from, name)
 		if err == nil {
 			routeCache.Store(from, a)
 			routeCache.Store(a.String(), a)
@@ -66,15 +92,15 @@ func (a *Router) Lookup(from string) (*identity.Address, error) {
 	return nil, errors.New("Couldn't find address in Trackers.")
 }
 
-func (a *Router) Register(key *identity.Identity, alias string) error {
+func (a *Router) Register(key *identity.Identity, alias string, redirect map[string]routing.Redirect) error {
 	var err error
 	success := 0
 
 	for _, v := range a.TrackerList {
-		newErr := (&tracker.TrackerRouter{
+		newErr := (&tracker.Router{
 			Origin: a.Origin,
 			URL:    v,
-		}).Register(key, alias)
+		}).Register(key, alias, redirect)
 		if newErr == nil {
 			success++
 		} else {
